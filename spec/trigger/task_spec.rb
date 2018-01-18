2d8abf1cadbc5ef496c8604994f00e5eecc81b4c
Telmo Costa <telmo@toptal.com>
2018-01-18 18:54:45 +0000

    Use a consistent string quotes usage
    Single quote usage was chosen, but we could use double quotes

--- spec/trigger/task_spec.rb
+++ spec/trigger/task_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Trigger::Task do
     pair programming with %s at next day
   TEMPLATE
 
-  it "has a version number" do
+  it 'has a version number' do
     expect(Trigger::Task::VERSION).not_to be nil
   end
 
-  describe ".parse" do
+  describe '.parse' do
     subject do
       Trigger::Task.parse template
     end
@@ -24,28 +24,28 @@ RSpec.describe Trigger::Task do
     end
   end
 
-  describe "slack_reminders_for" do
+  describe 'slack_reminders_for' do
     subject do
-      Trigger::Task.slack_reminders_for("@someone",
-        "invite %s to slack"=> Time.mktime(2018,01,15)
+      Trigger::Task.slack_reminders_for('@someone',
+        'invite %s to slack'=> Time.mktime(2018,01,15)
       )
     end
 
     specify do
-      is_expected.to eq ["/remind me to invite @someone to slack on 01/15/2018"]
+      is_expected.to eq ['/remind me to invite @someone to slack on 01/15/2018']
     end
   end
 
-  describe ".load_reminders" do
+  describe '.load_reminders' do
     subject do
       Trigger::Task.load_reminders('@user', template, now: Time.mktime(2018,1,16))
     end
 
     specify do
       is_expected.to eq([
-        "/remind me to invite @user to slack on 01/15/2018",
-        "/remind me to add @user to our team on 01/16/2018",
-        "/remind me to pair programming with @user on 01/17/2018"
+        '/remind me to invite @user to slack on 01/15/2018',
+        '/remind me to add @user to our team on 01/16/2018',
+        '/remind me to pair programming with @user on 01/17/2018'
       ])
     end
   end
@@ -59,14 +59,14 @@ RSpec.describe Trigger::Task do
     after { File.delete('newcomer.txt') }
 
     subject do
-      described_class.reminders_for("newcomer.txt", "@someone", now: Time.mktime(2018,1,21))
+      described_class.reminders_for('newcomer.txt', '@someone', now: Time.mktime(2018,1,21))
     end
 
     specify do
       is_expected.to eq([
-        "/remind me to invite @someone to slack on 01/20/2018",
-        "/remind me to add @someone to our team on 01/21/2018",
-        "/remind me to pair programming with @someone on 01/22/2018"
+        '/remind me to invite @someone to slack on 01/20/2018',
+        '/remind me to add @someone to our team on 01/21/2018',
+        '/remind me to pair programming with @someone on 01/22/2018'
       ])
     end
   end
